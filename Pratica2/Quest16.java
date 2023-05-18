03d0d8ea318426798f2a576935a270af6b5b82a4
Manoel-DJS <vinesouza9x8@gmail.com>
2023-05-17 21:36:04 -0300

    Updade

--- Pratica2/Quest16.java
+++ Pratica2/Quest16.java
@@ -24,5 +24,6 @@ public class Quest16 {
             numero = scanner.nextInt();
         }
 
+        scanner.close();
     }    
 }
---
+++ Pratica2/Quest17.java
@@ -0,0 +1,18 @@
+/**
+ 
+Nome do Aluno: Manoel Vinicius Silva Souza
+RA: 722315125
+Data: 21 / 05 / 2023
+
+Nome do Programa:
+17. Faça um programa que imprima os múltiplos negativos de 7,
+superiores a -1000. (utilize do / while)
+
+**/
+package Pratica2;
+
+public class Quest17 {
+    public static void main(String[] args) {
+        
+    }
+}
